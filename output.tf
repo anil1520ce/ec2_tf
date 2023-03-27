14a6fce21c055f3113cde6ddcb0ae8e2d4e3a16b
Anil <manilkumar.ce@gmail.com>
2023-03-27 16:36:58 +0530

    second commit on develop branch

---
+++ output.tf
@@ -0,0 +1,13 @@
+output "test_ec2_id" {
+  description = "Print the ID of EC2 Instance"
+  value       = i-007354e525faf6f27
+}
+
+output "test_ec2_private_ip" {
+  description = "Print the Private IP of EC2 Instance"
+  value       = 172.31.81.10
+
+output "test_ec2_public_ip" {
+  description = "Print the Public IP of EC2 Instance"
+  value       = 3.86.12.163
+}
---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+ami_id = "ami-00c39f71452c08778"
+inst_type = "t2.micro"
+key_name = "demokey"
+sg_ids = ["sg-063c8f999a87e9063"]
+subnet_id = "subnet-0c95f32ef3dfcf982"
+region = "us-east-1"
